23f20040fc5969dc7a1315ccf4d27c9096053e6d
pranberry <180306401+pranberry@users.noreply.github.com>
2025-09-30 23:53:24 -0400

    logging now adds statuscode on response log

--- handlers/response.go
+++ handlers/response.go
@@ -1,18 +1,17 @@
 package handlers
 
 import (
-	"encoding/json"
 	"auth-api/models"
+	"encoding/json"
 	"net/http"
 )
 
-func writeResponse(w http.ResponseWriter, resp models.Response) {
-
+// Shared function to write json out to the
+func WriteResponse(w http.ResponseWriter, resp *models.Response) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(resp.Status)
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		resp.Error = err
 		http.Error(w, resp.Message, resp.Status)
 	}
-	// Add log here
 }
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,42 +1,73 @@
 package middleware
 
 import (
-	"fmt"
 	"auth-api/auth"
+	"auth-api/handlers"
+	"auth-api/models"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
 )
 
+// Embedding is awesome!
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (r *statusRecorder) WriteHeader(status int) {
+	r.status = status
+	r.ResponseWriter.WriteHeader(status)
+}
+
 func Logger(next http.HandlerFunc) http.HandlerFunc {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("REQUEST: %s, on %s, from %v", r.Method, r.URL.Path, r.RemoteAddr)
-		next.ServeHTTP(w, r)
-		log.Printf("RESPONSE: HTTPHEADERVALUEHERE on %s, on %s, from %v", r.Method, r.URL.Path, r.RemoteAddr)
+
+		rec := &statusRecorder{ResponseWriter: w, status: http.StatusOK}
+		next.ServeHTTP(rec, r)
+
+		log.Printf("RESPONSE: %d on %s, on %s, from %v", rec.status, r.Method, r.URL.Path, r.RemoteAddr)
 		// how do i get the http.status sent from the request or write object?
 	})
 }
 
+// Checks for an Authorization header and validates the token
 func CheckJwt(next http.HandlerFunc) http.HandlerFunc {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		resp := models.Response{
+			Status:  http.StatusUnauthorized,
+			Message: "failed to validate auth token",
+			Error:   nil,
+			Data:    nil,
+		}
+
+		// write response on fail
+		defer handlers.WriteResponse(w, &resp)
+
+		// retrieve header
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			http.Error(w, "no auth token", http.StatusUnauthorized)
+			resp.Error = fmt.Errorf("no auth token")
+			resp.Message = resp.Error.Error()
+			resp.Status = http.StatusBadRequest
 			return
 		}
 		authHeader, ok := strings.CutPrefix(authHeader, "Bearer ")
 		if !ok {
-			log.Println("corrupt token format")
-			http.Error(w, "corrupt token format", http.StatusUnauthorized)
+			resp.Error = fmt.Errorf("corrupt token format")
+			resp.Message = resp.Error.Error()
+			resp.Status = http.StatusBadRequest
 			return
 		}
 		err := auth.ValidateJWT(authHeader)
 		if err != nil {
-			err = fmt.Errorf("error validating token: %w", err)
-			http.Error(w, err.Error(), http.StatusUnauthorized)
+			resp.Error = fmt.Errorf("error validating token: %w", err)
+			resp.Message = resp.Error.Error()
 			return
 		}
 
